90882a6924409cd2978f898b8cad0ad7e7f61141
CodeLaboratory-Patrick <gptnc5100@gmail.com>
2024-11-26 21:37:10 +0100

    Adding a model sheet & Understanding context

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -26,6 +26,10 @@ class _ExpensesState extends State<Expenses> {
         category: Category.leisure),
   ];
 
+  void _openAddExpenseOverlay() {
+    showModalBottomSheet(context: context, builder: (ctx) => Text('Modal bottom sheet'));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -33,7 +37,7 @@ class _ExpensesState extends State<Expenses> {
         title: const Text('Flutter ExpenseTracker'),
         actions: [
           IconButton(
-            onPressed: () {}, 
+            onPressed: _openAddExpenseOverlay, 
             icon: const Icon(Icons.add),
             )
         ],
