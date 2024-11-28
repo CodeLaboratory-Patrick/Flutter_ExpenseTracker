e1413dca560cca3a5639c74ff1f8262446c3eb35
CodeLaboratory-Patrick <gptnc5100@gmail.com>
2024-11-28 20:09:32 +0100

    Using Theme Data in Widgets.

--- lib/widgets/expenses_list/expenses_list.dart
+++ lib/widgets/expenses_list/expenses_list.dart
@@ -19,13 +19,15 @@ class ExpensesList extends StatelessWidget {
       itemCount: expenses.length,
       itemBuilder: (ctx, index) => Dismissible(
         key: ValueKey(expenses[index]),
+        background: Container(
+          color: Theme.of(context).colorScheme.error.withOpacity(0.75),
+          margin: EdgeInsets.symmetric(
+              horizontal: Theme.of(context).cardTheme.margin!.horizontal),
+        ),
         onDismissed: (direction) {
-          onRemoveExpense(
-            expenses[index]);
+          onRemoveExpense(expenses[index]);
         },
-        child: ExpenseItem(
-          expenses[index]
-          ),
+        child: ExpenseItem(expenses[index]),
       ),
     );
   }
