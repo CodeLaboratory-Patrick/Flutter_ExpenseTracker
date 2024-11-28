28e5c8cc325267d67fdc8a005286cd9617a28685
CodeLaboratory-Patrick <gptnc5100@gmail.com>
2024-11-28 11:25:14 +0100

    Saving new Expenses

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:expense_tracker/models/expense.dart';
 
 class NewExpense extends StatefulWidget {
-  const NewExpense({super.key});
+  const NewExpense({super.key, required this.onAddExpense});
+
+  final void Function(Expense expense) onAddExpense;
 
   @override
   State<NewExpense> createState() {
@@ -45,17 +47,23 @@ class _NewExpenseState extends State<NewExpense> {
               'Please make sure a valid title, amount, date, and category was entered.'),
           actions: [
             TextButton(
-                onPressed: () {
-                  Navigator.pop(ctx);
-                },
-                child: const Text('Okay'),
+              onPressed: () {
+                Navigator.pop(ctx);
+              },
+              child: const Text('Okay'),
             ),
           ],
         ),
       );
       return;
     }
-    
+    widget.onAddExpense(
+      Expense(
+          title: _titleController.text,
+          amount: enteredAmount,
+          date: _selectedDate!,
+          category: _selectedCategory),
+    );
   }
 
   @override
