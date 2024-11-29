6d0fa2199bfbd47f7f80074878d96e53274c3c15
CodeLaboratory-Patrick <gptnc5100@gmail.com>
2024-11-29 15:56:28 +0100

    Looking the device orientation

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+// import 'package:flutter/services.dart';
 
 import 'package:expense_tracker/widgets/expenses.dart';
 
@@ -12,54 +13,58 @@ var kDarkColorScheme = ColorScheme.fromSeed(
 );
 
 void main() {
-  runApp(
-    MaterialApp(
-        darkTheme: ThemeData.dark().copyWith(
-          useMaterial3: true,
-          colorScheme: kDarkColorScheme,
-          cardTheme: const CardTheme().copyWith(
-            color: kDarkColorScheme.secondaryContainer,
-            margin: const EdgeInsets.symmetric(
-              horizontal: 16,
-              vertical: 8,
-            ),
-          ),
-          elevatedButtonTheme: ElevatedButtonThemeData(
-            style: ElevatedButton.styleFrom(
-              backgroundColor: kDarkColorScheme.primaryContainer,
-              foregroundColor: kDarkColorScheme.onPrimaryContainer,
-            ),
-          ),
-        ),
-        
-        theme: ThemeData().copyWith(
-          useMaterial3: true,
-          colorScheme: kColourScheme,
-          appBarTheme: const AppBarTheme().copyWith(
-            backgroundColor: kColourScheme.onPrimaryContainer,
-            foregroundColor: kColourScheme.primaryContainer,
-          ),
-          cardTheme: const CardTheme().copyWith(
-            color: kColourScheme.secondaryContainer,
-            margin: const EdgeInsets.symmetric(
-              horizontal: 16,
-              vertical: 8,
-            ),
-          ),
-          elevatedButtonTheme: ElevatedButtonThemeData(
-            style: ElevatedButton.styleFrom(
-              backgroundColor: kColourScheme.primaryContainer,
-            ),
-          ),
-          textTheme: ThemeData().textTheme.copyWith(
-                titleLarge: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  color: kColourScheme.onSecondaryContainer,
-                  fontSize: 16,
-                ),
+  // WidgetsFlutterBinding.ensureInitialized();
+  // SystemChrome.setPreferredOrientations([
+  //   DeviceOrientation.portraitUp,
+  // ]).then((fn) {
+    runApp(
+      MaterialApp(
+          darkTheme: ThemeData.dark().copyWith(
+            useMaterial3: true,
+            colorScheme: kDarkColorScheme,
+            cardTheme: const CardTheme().copyWith(
+              color: kDarkColorScheme.secondaryContainer,
+              margin: const EdgeInsets.symmetric(
+                horizontal: 16,
+                vertical: 8,
               ),
-        ),
-        themeMode: ThemeMode.system,
-        home: const Expenses()),
-  );
+            ),
+            elevatedButtonTheme: ElevatedButtonThemeData(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: kDarkColorScheme.primaryContainer,
+                foregroundColor: kDarkColorScheme.onPrimaryContainer,
+              ),
+            ),
+          ),
+          theme: ThemeData().copyWith(
+            useMaterial3: true,
+            colorScheme: kColourScheme,
+            appBarTheme: const AppBarTheme().copyWith(
+              backgroundColor: kColourScheme.onPrimaryContainer,
+              foregroundColor: kColourScheme.primaryContainer,
+            ),
+            cardTheme: const CardTheme().copyWith(
+              color: kColourScheme.secondaryContainer,
+              margin: const EdgeInsets.symmetric(
+                horizontal: 16,
+                vertical: 8,
+              ),
+            ),
+            elevatedButtonTheme: ElevatedButtonThemeData(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: kColourScheme.primaryContainer,
+              ),
+            ),
+            textTheme: ThemeData().textTheme.copyWith(
+                  titleLarge: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: kColourScheme.onSecondaryContainer,
+                    fontSize: 16,
+                  ),
+                ),
+          ),
+          themeMode: ThemeMode.system,
+          home: const Expenses()),
+    );
+//  });
 }
