64713fdda748e39a86dbfda45be393a3add0df17
CodeLaboratory-Patrick <gptnc5100@gmail.com>
2024-11-29 16:11:40 +0100

    Updating the UI based on the available space.

--- lib/main.dart
+++ lib/main.dart
@@ -17,54 +17,54 @@ void main() {
   // SystemChrome.setPreferredOrientations([
   //   DeviceOrientation.portraitUp,
   // ]).then((fn) {
-    runApp(
-      MaterialApp(
-          darkTheme: ThemeData.dark().copyWith(
-            useMaterial3: true,
-            colorScheme: kDarkColorScheme,
-            cardTheme: const CardTheme().copyWith(
-              color: kDarkColorScheme.secondaryContainer,
-              margin: const EdgeInsets.symmetric(
-                horizontal: 16,
-                vertical: 8,
-              ),
-            ),
-            elevatedButtonTheme: ElevatedButtonThemeData(
-              style: ElevatedButton.styleFrom(
-                backgroundColor: kDarkColorScheme.primaryContainer,
-                foregroundColor: kDarkColorScheme.onPrimaryContainer,
-              ),
+  runApp(
+    MaterialApp(
+        darkTheme: ThemeData.dark().copyWith(
+          useMaterial3: true,
+          colorScheme: kDarkColorScheme,
+          cardTheme: const CardTheme().copyWith(
+            color: kDarkColorScheme.secondaryContainer,
+            margin: const EdgeInsets.symmetric(
+              horizontal: 16,
+              vertical: 8,
             ),
           ),
-          theme: ThemeData().copyWith(
-            useMaterial3: true,
-            colorScheme: kColourScheme,
-            appBarTheme: const AppBarTheme().copyWith(
-              backgroundColor: kColourScheme.onPrimaryContainer,
-              foregroundColor: kColourScheme.primaryContainer,
+          elevatedButtonTheme: ElevatedButtonThemeData(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: kDarkColorScheme.primaryContainer,
+              foregroundColor: kDarkColorScheme.onPrimaryContainer,
             ),
-            cardTheme: const CardTheme().copyWith(
-              color: kColourScheme.secondaryContainer,
-              margin: const EdgeInsets.symmetric(
-                horizontal: 16,
-                vertical: 8,
-              ),
+          ),
+        ),
+        theme: ThemeData().copyWith(
+          useMaterial3: true,
+          colorScheme: kColourScheme,
+          appBarTheme: const AppBarTheme().copyWith(
+            backgroundColor: kColourScheme.onPrimaryContainer,
+            foregroundColor: kColourScheme.primaryContainer,
+          ),
+          cardTheme: const CardTheme().copyWith(
+            color: kColourScheme.secondaryContainer,
+            margin: const EdgeInsets.symmetric(
+              horizontal: 16,
+              vertical: 8,
             ),
-            elevatedButtonTheme: ElevatedButtonThemeData(
-              style: ElevatedButton.styleFrom(
-                backgroundColor: kColourScheme.primaryContainer,
-              ),
+          ),
+          elevatedButtonTheme: ElevatedButtonThemeData(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: kColourScheme.primaryContainer,
             ),
-            textTheme: ThemeData().textTheme.copyWith(
-                  titleLarge: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    color: kColourScheme.onSecondaryContainer,
-                    fontSize: 16,
-                  ),
+          ),
+          textTheme: ThemeData().textTheme.copyWith(
+                titleLarge: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: kColourScheme.onSecondaryContainer,
+                  fontSize: 16,
                 ),
-          ),
-          themeMode: ThemeMode.system,
-          home: const Expenses()),
-    );
+              ),
+        ),
+        themeMode: ThemeMode.system,
+        home: const Expenses()),
+  );
 //  });
 }
--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -69,6 +69,8 @@ class _ExpensesState extends State<Expenses> {
 
   @override
   Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width;
+
     Widget mainContent = const Center(
       child: Text('No expenses found. Start adding some!'),
     );
@@ -90,15 +92,22 @@ class _ExpensesState extends State<Expenses> {
           )
         ],
       ),
-      body: Column(
-        children: [
-          //Toolbar with the Add button => Row()
-          Chart(expenses: _registeredExpenses),
-          Expanded(
-            child: mainContent
+      body: width < 600
+          ? Column(
+              children: [
+                //Toolbar with the Add button => Row()
+                Chart(expenses: _registeredExpenses),
+                Expanded(child: mainContent),
+              ],
+            )
+          : Row(
+              children: [
+                Expanded(
+                  child: Chart(expenses: _registeredExpenses),
+                ),
+                Expanded(child: mainContent),
+              ],
             ),
-        ],
-      ),
     );
   }
 }
